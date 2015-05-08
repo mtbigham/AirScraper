7c23b19d68af0afd71f2ae604b1d0f97b9d566b1
Taylor Bigham <mtbigham@ufl.edu>
2015-05-08 15:02:20 -0400

    Item Definitions

--- AirScraper/items.py
+++ AirScraper/items.py
@@ -9,6 +9,12 @@ import scrapy
 
 
 class AirscraperItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    depart = scrapy.Field()
+    dest = scrapy.Field()
+    departDate = scrapy.Field()
+    arriveDate = scrapy.Field()
+    flightNum = scrapy.Field()
+    departTime = scrapy.Field()
+    arriveTime = scrapy.Field()
+    price = scrapy.Field()
+    points = scrapy.Field()
